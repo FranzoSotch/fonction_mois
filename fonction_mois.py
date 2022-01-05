e7d4c10ef76e6586c4e6768bc07328475f02f43c
FranzoSotch <96664172+FranzoSotch@users.noreply.github.com>
2022-01-05 12:18:38 +0100

    Add files via upload

---
+++ fonction_mois.py
@@ -0,0 +1,8 @@
+def affiche_mois(numero):
+    if numero==1:
+        print("Nous sommes en Janvier.")
+    elif numero==2:
+        print("Nous sommes en Février.")
+    elif numero==3:
+        print("Nous sommes en Mars.")
+    return
